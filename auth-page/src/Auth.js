80a4b23d957d3c124fb1bbe2756fafd5bba94afb
Grace Wakio <gigiwakish04@gmail.com>
2025-02-16 12:25:47 +0300

    updated changes

--- auth-page/src/Auth.js
+++ auth-page/src/Auth.js
@@ -6,10 +6,33 @@ const Auth = () => {
         password: '',
         email: '',
         isLogin: true
-    })
+    });
+
+    const handleChange = (e) => {
+        const { name , value } = e.target;
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(formData);
+        // Here would be where you'd send data to the server for login or registration
+        setFormData({ username: '', password: '', email: '' });
+      };
+
+      const toggleForm = () => {
+        setFormData(prevState => ({
+          ...prevState,
+          isLogin: !prevState.isLogin,
+          email: ''
+        }));
+      };
 
     return (
-        <div>
+        <div className="auth-container">
             <h2>{formData.isLogin ? 'LOGIN' : 'REGISTER'}</h2>
             <form onSubmit={handleSubmit}>
                 <div>
